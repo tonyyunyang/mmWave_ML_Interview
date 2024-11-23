65d5dd59eca25eda542eafd75b84e49bc134fcc7
Your Name <your.email@example.com>
2024-11-23 17:39:30 +0100

    gif updated

--- modules/dataset.py
+++ modules/dataset.py
@@ -3,8 +3,9 @@ import einops
 
 
 class SignatureFigDataset(Dataset):
-    def __init__(self, signature_figs):
+    def __init__(self, signature_figs, filenames):
         self.signature_figs = signature_figs
+        self.filenames = filenames
 
     def __len__(self):
         return len(self.signature_figs)
@@ -12,4 +13,4 @@ class SignatureFigDataset(Dataset):
     def __getitem__(self, idx):
         signature_fig = self.signature_figs[idx] # (H, W, C)
         signature_fig = einops.rearrange(signature_fig, "h w c -> c h w")
-        return signature_fig / 255.0
+        return signature_fig / 255.0, self.filenames[idx]
